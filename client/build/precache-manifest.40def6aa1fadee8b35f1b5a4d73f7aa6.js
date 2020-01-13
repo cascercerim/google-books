8056d915341e6a8adda0b67b7c8396207819501c
Maria <cascercerim@gmail.com>
2020-01-13 17:16:02 -0500

    initial commit

---
+++ client/build/precache-manifest.40def6aa1fadee8b35f1b5a4d73f7aa6.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ed69d9294cf98fa0488b2b351ecbb8c4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7e4da1a788fd8adcf0d5",
+    "url": "/static/css/2.47e06e2e.chunk.css"
+  },
+  {
+    "revision": "f04471aaeeb969d6caa4",
+    "url": "/static/css/main.af0674b4.chunk.css"
+  },
+  {
+    "revision": "7e4da1a788fd8adcf0d5",
+    "url": "/static/js/2.a67de788.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/static/js/2.a67de788.chunk.js.LICENSE"
+  },
+  {
+    "revision": "f04471aaeeb969d6caa4",
+    "url": "/static/js/main.d68179d8.chunk.js"
+  },
+  {
+    "revision": "0281e3aa3ea9fc02c242",
+    "url": "/static/js/runtime-main.b12ff581.js"
+  },
+  {
+    "revision": "635065a940cf2e414bb0a42a0db861ff",
+    "url": "/static/media/book1.635065a9.jpg"
+  }
+]);
