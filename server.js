5353a737f296da9286906f2438d2d685af71eff2
Maria <cascercerim@gmail.com>
2020-01-13 18:00:43 -0500

    initial commit

--- server.js
+++ server.js
@@ -4,6 +4,10 @@ const mongoose = require("mongoose");
 const routes = require("./routes");
 const app = express();
 
+  // Add routes, both API and view
+  app.use(routes);
+  const PORT = process.env.PORT || 3001;
+
 // Define middleware here
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
@@ -17,9 +21,7 @@ if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
   }
 
-  // Add routes, both API and view
-app.use(routes);
-const PORT = process.env.PORT || 3001;
+
 // Start the API server
 app.listen(PORT, function() {
   console.log(`🌎  ==> API Server now listening on PORT ${PORT}!`);
