9e356922ce176d055f9ed2fd338fb1258c9487a2
Maria <cascercerim@gmail.com>
2020-01-13 22:55:46 -0500

    trying to upload to heroku

--- server.js
+++ server.js
@@ -3,22 +3,26 @@ const express = require("express");
 const mongoose = require("mongoose");
 const routes = require("./routes");
 const app = express();
+const log = console.log;
 
   // Add routes, both API and view
   app.use(routes);
-  const PORT = process.env.PORT || 3001;
+  const PORT = process.env.PORT || 8080;
 
 // Define middleware here
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 // Connect to the Mongo DB
-mongoose.connect(process.env.MONGODB_URI || "mongodb://cascercerim:Kaylam9!@ds263248.mlab.com:63248/heroku_r2hx023g");
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/google-books");
 console.log('MongoDB connected');
 
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
   }
+  app.get('*',(req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
 
   
 // Start the API server
